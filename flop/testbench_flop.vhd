3a97597f224a43d3236bebf6f6af8d4fe9ddf732
Vinicius Medeiros Wanderley <31319885+ViniciusMedeirosWanderley@users.noreply.github.com>
2018-04-20 08:20:49 -0300

    Preparação 5

---
+++ flop/testbench_flop.vhd
@@ -0,0 +1,96 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_arith.ALL;
+use IEEE.STD_LOGIC_unsigned.ALL;
+use STD.TEXTIO.ALL ;
+
+entity flop is -- no inputs or outputs
+end;
+architecture sim of flop is
+	component flop
+		port(clk: in STD_LOGIC;
+         	 d: in STD_LOGIC_VECTOR(3 downto 0);
+         	 q: out STD_LOGIC_VECTOR(3 downto 0));
+	end component;
+signal clk: STD_LOGIC;
+signal d: STD_LOGIC_VECTOR(3 downto 0);
+signal yexpected: STD_LOGIC_VECTOR(3 downto 0);
+constant MEMSIZE: integer := 32; --colocar numero de linhas aqui que eu vou escolher.
+type tvarray is array (MEMSIZE downto 0) of
+STD_LOGIC_VECTOR (8 downto 0); --número de colunas começando a contar do 0.
+signal testvectors: tvarray;
+shared variable vectornum, errors: integer;
+begin
+-- instantiate device under test
+dut: flop port map (clk, d, q);
+-- generate clock
+process begin
+	clk <= '1'; wait for 15 ns;  --mudar esses números.
+	clk <= '0'; wait for 5 ns;  --mudar esses números.
+end process;
+-- at start of test, load vectors
+-- and pulse reset
+process is
+file tv: TEXT;
+variable i, j: integer;
+variable L: line;
+variable ch: character;
+begin
+	-- read file of test vectors
+	i := 0;
+	FILE_OPEN (tv, "./flop.tv", READ_MODE);
+	while not endfile(tv) loop
+		readline (tv, L);
+		for j in 8 downto 0 loop --número de colunas.
+			read (L, ch);
+			if (ch = '_') then read (L, ch);
+			end if;
+			if (ch = '0') then
+			testvectors (i) (j) <= '0';
+			else testvectors (i) (j) <= '1';
+			end if;
+		end loop;
+		i := i + 1;
+	end loop;
+	vectornum := 0; errors := 0;
+	-- reset <= '1'; wait for 27 ns; reset <= '0';
+	wait;
+end process;
+-- apply test vectors on rising edge of clk
+process (clk) begin
+	if (clk'event and clk='1') then
+		d <= testvectors (vectornum) (8 downto 5);
+		clk <= testvectors (vectornum) (4);
+		yexpected <= testvectors (vectornum)(3 downto 0); --numero de colunas da tabela verdade começando da direita.
+	end if;
+end process;
+-- check results on falling edge of clk
+process (clk) begin
+	if (clk'event and clk = '0')then
+		for k in 0 to 3 loop
+			assert y(k)= yexpected(k)
+				report "Vetor deu erro n. Teste: " &integer'image(vectornum)&". Esperado yesp ="& STD_LOGIC'image(yexpected(k))&"Valor Obtido: y1("&integer'image(k)&") ="& STD_LOGIC'image(y(k));
+			
+			if (y /= yexpected) then
+				errors := errors + 1;
+			end if;
+		end loop;
+		
+		vectornum := vectornum + 1;
+		--if (is_x (testvectors(vectornum))) then
+			if (errors = 0) then
+				--report "Just kidding --" &
+				integer'image (vectornum) &
+				"tests completed successfully."
+				severity failure;
+			else
+				report integer'image (vectornum) &
+				"tests completed, errors = " &
+				integer'image (errors)
+				severity failure;
+			end if;
+		end if;
+	end if;
+	
+end process;
+end;
---
+++ sincronizador - sync/testbench_sync.vhd
@@ -0,0 +1,96 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_arith.ALL;
+use IEEE.STD_LOGIC_unsigned.ALL;
+use STD.TEXTIO.ALL ;
+
+entity sync is -- no inputs or outputs
+end;
+architecture sim of sync is
+	component sync
+		port(clk: in STD_LOGIC;
+         	 d: in STD_LOGIC;
+         	 q: out STD_LOGIC);
+	end component;
+signal clk: STD_LOGIC;
+signal d: STD_LOGIC;
+signal yexpected: STD_LOGIC;
+constant MEMSIZE: integer := 4; --colocar numero de linhas aqui que eu vou escolher.
+type tvarray is array (MEMSIZE downto 0) of
+STD_LOGIC_VECTOR (2 downto 0); --número de colunas começando a contar do 0.
+signal testvectors: tvarray;
+shared variable vectornum, errors: integer;
+begin
+-- instantiate device under test
+dut: latch port map (clk, d, q);
+-- generate clock
+process begin
+	clk <= '1'; wait for 15 ns;  --mudar esses números.
+	clk <= '0'; wait for 5 ns;  --mudar esses números.
+end process;
+-- at start of test, load vectors
+-- and pulse reset
+process is
+file tv: TEXT;
+variable i, j: integer;
+variable L: line;
+variable ch: character;
+begin
+	-- read file of test vectors
+	i := 0;
+	FILE_OPEN (tv, "./sync.tv", READ_MODE);
+	while not endfile(tv) loop
+		readline (tv, L);
+		for j in 2 downto 0 loop --número de colunas.
+			read (L, ch);
+			if (ch = '_') then read (L, ch);
+			end if;
+			if (ch = '0') then
+			testvectors (i) (j) <= '0';
+			else testvectors (i) (j) <= '1';
+			end if;
+		end loop;
+		i := i + 1;
+	end loop;
+	vectornum := 0; errors := 0;
+	-- reset <= '1'; wait for 27 ns; reset <= '0';
+	wait;
+end process;
+-- apply test vectors on rising edge of clk
+process (clk) begin
+	if (clk'event and clk='1') the
+		d <= testvectors (vectornum) (2);
+		clk <= testvectors (vectornum) (1);
+		yexpected <= testvectors (vectornum)(0); --numero de colunas da tabela verdade começando da direita.
+	end if;
+end process;
+-- check results on falling edge of clk
+process (clk) begin
+	if (clk'event and clk = '0')then
+		for k in 0 to 3 loop
+			assert y(k)= yexpected(k)
+				report "Vetor deu erro n. Teste: " &integer'image(vectornum)&". Esperado yesp ="& STD_LOGIC'image(yexpected(k))&"Valor Obtido: y1("&integer'image(k)&") ="& STD_LOGIC'image(y(k));
+			
+			if (y /= yexpected) then
+				errors := errors + 1;
+			end if;
+		end loop;
+		
+		vectornum := vectornum + 1;
+		--if (is_x (testvectors(vectornum))) then
+			if (errors = 0) then
+				--report "Just kidding --" &
+				integer'image (vectornum) &
+				"tests completed successfully."
+				severity failure;
+			else
+				report integer'image (vectornum) &
+				"tests completed, errors = " &
+				integer'image (errors)
+				severity failure;
+			end if;
+		end if;
+	end if;
+	
+end process;
+end;
